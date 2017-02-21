7f33f4e47c232fcc9179f9e1d13a513b64f161e0
Rick Lee <eerick903@gmail.com>
2017-02-21 23:38:23 +0800

    [Apache Wicket Tutorial] #2 - Wicket Link & navigate to the second page

--- wicket/src/main/java/net/rickcodetalk/wicket/page/HomePage.java
+++ wicket/src/main/java/net/rickcodetalk/wicket/page/HomePage.java
@@ -1,10 +1,21 @@
 package net.rickcodetalk.wicket.page;
 
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.WebPage;
 
 public class HomePage extends WebPage {	
     public HomePage() {
-	add(new Label("helloMessage", "rickcodetalk Wicket tut!"));
+    		add(new Label("helloMessage", "rickcodetalk Wicket tutorial!"));
+    		
+    		add(new Link("secondPageLink") {
+
+				@Override
+				public void onClick() {
+					// TODO Auto-generated method stub
+					setResponsePage(SecondPage.class);
+				}
+    			
+    		});
     }
 }
---
+++ wicket/src/main/java/net/rickcodetalk/wicket/page/SecondPage.java
@@ -0,0 +1,11 @@
+package net.rickcodetalk.wicket.page;
+
+import org.apache.wicket.markup.html.WebPage;
+
+public class SecondPage extends WebPage {
+	
+	public SecondPage() {
+		
+	}
+	
+}
